69dd736379a9c434ca466f49ccf3ef0697ee290e
takkorai <takkorai@gmail.com>
2014-04-20 12:31:16 -0400

    unit 2 lesson 3 completed

--- js/app.js
+++ js/app.js
@@ -1,23 +1,59 @@
 $(document).ready(function(){
+
 	$(".ryu").mouseenter(function(){
-		/*alert('mouseentered .ryu div')*/
+		
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
+		$('.ryu-cool').hide();
+		$('.ryu-throwing').hide();
 	})
 	.mouseleave(function(){
 		$('.ryu-still').show();
 		$('.ryu-ready').hide();
+		$('.ryu-cool').hide();
+		$('.ryu-throwing').hide();
 	})
 	.mousedown(function(){
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-    	// play hadouken sound
-    	// show hadouken and animate it to the right of the screen
+		$('.ryu-cool').hide();
+		$('.hadouken').finish().show()
+    		.animate({"left":"300px"},500,function(){
+	    		$(this).hide();
+	    		$(this).css('left','-212px');
+    		});
 	})
 	.mouseup(function(){
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-		// ryu goes back to his ready position
+		$('.ryu-cool').hide();
+
+	});
+
+	$(document).keydown(function(e){
+		if(e.keyCode==88){
+			
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-cool').show();
+
+		}
 	})
+
+	.keyup(function(){
+		$('.ryu-cool').hide();
+		$('.ryu-still').show();
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').hide();
+	});
+
 });
+
+
+function playHadouken (){
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+};
